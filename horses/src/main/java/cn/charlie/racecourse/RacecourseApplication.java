6e10528e78ecfa6eedd0e64ab3db271b7545b59b
charlie <327371999@qq.com>
2023-03-14 23:38:12 +0800

    RACE-2 线程池初始化

---
+++ horses/src/main/java/cn/charlie/racecourse/RacecourseApplication.java
@@ -0,0 +1,15 @@
+package cn.charlie.racecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author charlie
+ * @date 3/14/2023 10:30 AM
+ **/
+@SpringBootApplication
+public class RacecourseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RacecourseApplication.class, args);
+    }
+}
---
+++ horses/src/main/java/cn/charlie/racecourse/countdownlatch/CountDownLatchClient.java
@@ -0,0 +1,9 @@
+package cn.charlie.racecourse.countdownlatch;
+
+/**
+ * @author charlie
+ * @date 3/13/2023 9:42 PM
+ **/
+public class CountDownLatchClient {
+
+}
