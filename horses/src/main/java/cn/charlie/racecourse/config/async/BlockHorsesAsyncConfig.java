5d02be251868ab75b4583fe57014ce9949d72e7b
charlie <327371999@qq.com>
2023-03-15 10:18:19 +0800

    RACE-2 线程池初始化

--- horses/src/main/java/cn/charlie/racecourse/config/async/BlockHorsesAsyncConfig.java
+++ horses/src/main/java/cn/charlie/racecourse/config/async/BlockHorsesAsyncConfig.java
@@ -13,11 +13,11 @@ import java.util.concurrent.ThreadPoolExecutor;
  **/
 @EnableAsync
 @Configuration
-public class HorsesAsyncConfig {
+public class BlockHorsesAsyncConfig {
     /**
      * 核心线程池大小
      */
-    private static final int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors();
+    private static final int CORE_POOL_SIZE = 99;
     /**
      * 最大可创建的线程数
      */
@@ -25,14 +25,14 @@ public class HorsesAsyncConfig {
     /**
      * 队列最大长度
      */
-    private static final int QUEUE_CAPACITY = 16;
+    private static final int QUEUE_CAPACITY = 1024;
     /**
      * 线程池维护线程所允许的空闲时间
      */
     private static final int KEEP_ALIVE_SECONDS = 60;
 
-    @Bean(name = "TODO")
-    public ThreadPoolTaskExecutor taskExecutor() {
+    @Bean(name = "blockHorsesExecutor")
+    public ThreadPoolTaskExecutor blockHorsesExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         // 核心线程池大小
         executor.setCorePoolSize(CORE_POOL_SIZE);
@@ -43,7 +43,7 @@ public class HorsesAsyncConfig {
         // 线程池维护线程所允许的空闲时间
         executor.setKeepAliveSeconds(KEEP_ALIVE_SECONDS);
         // 线程前缀名称
-        executor.setThreadNamePrefix("TODO");
+        executor.setThreadNamePrefix("block-horses-executor-thread-pool-");
         // 拒绝策略: 交给调用线程处理
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
