46c9cdbf0d067adb59e079eeb02903ba441928aa
charlie <327371999@qq.com>
2023-03-15 15:26:28 +0800

    RACE-4 集成测试框架

---
+++ horses/src/test/java/cn/charlie/racecourse/cpu/CpuThreadTest.java
@@ -0,0 +1,35 @@
+package cn.charlie.racecourse.cpu;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.core.annotation.Order;
+
+
+/**
+ * @author charlie
+ * @date 3/15/2023 2:03 PM
+ **/
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CpuThreadTest {
+
+    @Test
+    @Order(1)
+    public void oneThreadFullLoadTest() {
+        while (true) {
+        }
+    }
+
+    @Test
+    @Order(2)
+    public void eightThreadFullLoadTest() {
+        for (int j = 0; j < 7; j++) {
+            new Thread(() -> {
+                while (true){
+                }
+            }).start();
+        }
+        while (true) {
+        }
+    }
+}
