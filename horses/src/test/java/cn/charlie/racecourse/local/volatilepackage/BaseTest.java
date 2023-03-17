bd78872031c510792d9cdbb054713e1552dee666
charlie <327371999@qq.com>
2023-03-17 17:42:46 +0800

    RACE-12 volatile案例

--- horses/src/test/java/cn/charlie/racecourse/local/volatilepackage/BaseTest.java
+++ horses/src/test/java/cn/charlie/racecourse/local/volatilepackage/BaseTest.java
@@ -1,6 +1,5 @@
 package cn.charlie.racecourse.local.volatilepackage;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -17,7 +16,7 @@ public class BaseTest {
 
     @Test
     @Order(1)
-    public void testBase() throws InterruptedException, ExecutionException {
+    public void testBase() throws InterruptedException {
         new AppleOperation().buildAppleBox();
     }
 }
